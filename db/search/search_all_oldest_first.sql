5657c001d726f1be0943f16556560dca50727471
keplerridge <kepler.ridge@gmail.com>
2021-01-19 14:02:40 -0700

    finished authentication

---
+++ db/search/search_all_oldest_first.sql
@@ -0,0 +1,2 @@
+SELECT * FROM helo_posts hp
+JOIN helo_users hu ON
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -11,22 +11,43 @@ module.exports = {
         }
 
         const salt = bcrypt.genSaltSync(10),
-              hash = bcrypt.hashSync(password, salt)
-              registeredUser = await db.create_user([username, password, profilePic]);
+              hash = bcrypt.hashSync(password, salt),
+              registeredUser = await db.create_user([username, hash, profilePic]),
               user = registeredUser[0]
         req.session.user = {
             username: user.username,
             id: user.id
         }
-        return res.status(201).send(req.session.user)
+        return res.status(201).send(req.session.user);
     },
-    login: (req, res) => {
+    login: async (req, res) => {
+        const {username, password} = req.body,
+              db = req.app.get('db')
+              foundUser = db.find_user_by_username([username]),
+              user = foundUser[0];
 
+        if(!user){
+            return res.status(401).send('User not found, please create a profile to login');
+        };
+
+        const isAuthenticated = bcrypt.compareSync(password, user.hash);
+
+        if(!isAuthenticated){
+            return res.status(403).send('Incorrect Password');
+        };
+
+        req.session.user = {id: user.id, username: user.username};
+        return res.send(req.session.user)
     },
     getUser: (req, res) => {
+        if(req.session.user){
+            res.status(200).send(req.session.user)
+        }
 
+        return res.status(404);
     },
     logout: (req, res) => {
-
+        req.session.destroy();
+        return res.status(200);
     }
 }
